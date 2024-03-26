6be1cd4cf1ed0e8f29dad4f100b90f9bc981d337
Jananair30 <164132191+Jananair30@users.noreply.github.com>
2024-03-26 16:37:10 +0800

    Update gwas.py

--- gwas.py
+++ gwas.py
@@ -1,21 +1,17 @@
 import os
 from openai import OpenAI
 from PIL import Image
-from io import BytesIO
 import streamlit as st
 import requests
 from gwasMethods import gwasMethods as gwas
 
-client= OpenAI(
-    api_key = st.secrets["OPEN_API_KEY"]
-    )
+client = OpenAI(api_key=st.secrets["OPEN_API_KEY"])
 
 def display_image(image_url):
-    response = requests.get(image_url)
-    img = Image.open(response.content)
-    image_stream = BytesIO(image_data)
-    img = Image.open(image_stream)
-    st.image(img)
+    response = requests.get(image_url, stream=True)
+    if response.status_code == 200:
+        img = Image.open(response.raw)
+        st.image(img, caption='Cover Image')
 
 def main():
     st.title("Gym Workout Activity Suggestor (GWAS.ai)")
@@ -23,14 +19,12 @@ def main():
     # User input for workout suggestion
     workout_input = st.text_input("Enter your workout goal or body part focus:", "")
     if workout_input:
-
-        workout_suggestion = gwas.gym_ai(workout_input,client)
-        design_prompt = gwas.design_ai(workout_input,client)
-        image_url = gwas.cover_ai(design_prompt,client)
+        workout_suggestion = gwas.gym_ai(workout_input, client)
+        design_prompt = gwas.design_ai(workout_input, client)
+        image_url = gwas.cover_ai(design_prompt, client)
 
         st.write("Workout Suggestion:")
         st.write(workout_suggestion)
-
         st.divider()
 
     if st.button("Generate Cover Image"):
