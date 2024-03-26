2d67bee5a5fd9e05d80c223eb457bafb53c8b76b
Jananair30 <164132191+Jananair30@users.noreply.github.com>
2024-03-26 13:22:26 +0800

    added self

--- gwasMethods.py
+++ gwasMethods.py
@@ -1,6 +1,6 @@
 class gwasMethods:
 
-    def gym_ai(msg,client):
+    def gym_ai(self,msg,client):
         gym_response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -21,7 +21,7 @@ class gwasMethods:
 
         return gym
 
-    def cover_ai(msg,client):
+    def cover_ai(self,msg,client):
         cover_response = client.images.generate(
         model="dall-e-3",
         prompt=f"{msg}",
@@ -35,7 +35,7 @@ class gwasMethods:
 
         return image_url
 
-    def design_ai(msg,client):
+    def design_ai(self,msg,client):
         design_response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
